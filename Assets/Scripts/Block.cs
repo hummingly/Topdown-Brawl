f4bda601995c0aa3b7d23a5d5092a56a2f57ad09
Nito <nikolas1997@hotmail.de>
2019-10-08 18:26:21 +0200

    Environment Setup

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour // need mono?
+{
+    private Rigidbody2D rb;
+
+    void init(bool moveable, float weight, bool destructible, Sprite sprite)
+    {
+        if (moveable)
+        {
+            rb = gameObject.AddComponent<Rigidbody2D>();
+            rb.mass = weight;
+            gameObject.AddComponent<Collider2D>();
+        }
+    }
+
+
+
+    void Update()
+    {
+        
+    }
+}
