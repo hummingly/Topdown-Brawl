8cd757fcd477b8c6572541b1ba2c58e72c6b6c24
LittleBird <31522351+hummingly@users.noreply.github.com>
2019-10-10 13:49:49 +0200

    Shoots projectiles on pressing A/Button East * bullets do not get destroyed yet * relative position of gun is broken

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour
+{
+    public Rigidbody2D projectile;
+    public float speed = 4;
+
+    // Use this for initialization
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnA()
+    {
+        Debug.Log("SHOOT!");
+        Rigidbody2D p = Instantiate(projectile, transform.position, transform.rotation);
+        p.velocity = transform.forward * speed;
+    }
+}
