bd472c72ce319f64c706ec71d32c5ab9730574c9
Nito <nikolas1997@hotmail.de>
2019-11-29 18:06:36 +0100

    moaer sounds

--- Assets/Scripts/Player/SoundsPlayer.cs
+++ Assets/Scripts/Player/SoundsPlayer.cs
@@ -9,7 +9,8 @@ public class SoundsPlayer : MonoBehaviour
     public AudioSource damagedMuchClip;
     public AudioSource deathClip;
     public AudioSource bounceWallClip; //also on any bounce? maybe volume depending on speed
-
+    public AudioSource bounceWallClip2; //also on any bounce? maybe volume depending on speed
+    public AudioSource dashClip;
 
     void Start()
     {
@@ -35,5 +36,24 @@ public class SoundsPlayer : MonoBehaviour
     public void respawn()
     {
         respawnClip.Play();
+    }
+
+    public void dash()
+    {
+        dashClip.time = 0.2f;
+        dashClip.Play();
+    }
+
+    public void bounceWall(float speed)
+    {
+        //print(speed);
+        //play whichever bounce clip is free, if both free play one random
+        int rand = Random.Range(0, 2);
+        AudioSource toPlay = rand == 0 ? bounceWallClip : bounceWallClip2;
+        if (toPlay.isPlaying)
+            toPlay = rand == 1 ? bounceWallClip : bounceWallClip2;
+
+        toPlay.volume = speed * 0.05f;
+        toPlay.Play();
     }
 }
